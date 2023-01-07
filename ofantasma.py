cb977aef38bc8d5f98cf9e9e8bfd6a9a0ca7ccaa
DDF95 <difebbo.davide@gmail.com>
2023-01-07 22:34:04 +0100

    moved functions to other .py files

--- ofantasma.py
+++ ofantasma.py
@@ -10,9 +10,10 @@ from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
 from telegram.ext import (ApplicationBuilder, CallbackQueryHandler,
                           CommandHandler, ContextTypes, InlineQueryHandler,
                           MessageHandler, PicklePersistence, filters)
-from transmission_rpc import Client
 
-from TikTok_scraper import *
+from facebook import *
+from tiktok import *
+from transmission_torrent import *
 from utilities import *
 
 
@@ -196,19 +197,10 @@ async def link_downloader(update: Update, context: ContextTypes.DEFAULT_TYPE):
                     await update.message.reply_photo(photo=url, parse_mode='HTML', caption=f"@{username}")
 
         if update.message.text.startswith(("magnet:", "https://proxyrarbg.org/download.php")):
-                magnet_url = update.message.text
-                c = Client(host="localhost", port=9091, username="admin", password="Eddy95")
-                torrent = c.add_torrent(magnet_url)
-                await update.message.reply_text(f"ok ho messo a scaricare `{torrent.name}`.", parse_mode="Markdown")
-
-
-async def inline_tiktok_download(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.inline_query.query
-    if not query:
-        return
-
-    if query.startswith(("https://vm.tiktok.com", "https://www.tiktok.com")):
-        await send_tiktok_inline(update, context, query)
+            await add_torrent(update, context)
+        
+        if update.message.text.startswith(("https://fb.watch/", "https://www.facebook.com/reel/")):
+            await send_facebook_video(update, context)
 
 
 async def spongebob(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -353,97 +345,6 @@ async def delete(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.reply_to_message.message_id)
 
 
-async def get_torrent_list():
-    c = Client(host="localhost", port=9091, username="admin", password="Eddy95")
-    torrents = c.get_torrents()
-
-    import datetime
-    import time
-
-    if len(torrents) == 0:
-        all_torrents = "Non ci sono torrent in download."
-        return all_torrents
-    else:
-        all_torrents = ""
-        active_torrents = "Attivi nelle ultime 24 ore:\n\n"
-        download_speed = 0
-        upload_speed = 0
-
-        for torrent in torrents:
-            if torrent.rateDownload > 0: 
-                all_torrents += "⚡️ "
-            elif torrent.progress == 100:
-                all_torrents += "🎊 "
-            elif torrent.available == 0 and torrent.progress != 100:
-                all_torrents += "😡 "
-            elif torrent.available > 0 and torrent.progress != 100 and torrent.rateDownload == 0:
-                all_torrents += "🦦 "
-            torrent_name_fixed = torrent.name.replace(".", " ")
-
-            if torrent.rateDownload /1024 > 1024:
-                download_speed = f"{round(torrent.rateDownload / 1024 / 1024, 2)} MB/s"
-            else:
-                download_speed = f"{round(torrent.rateDownload / 1024, 2)} KB/s"
-            if torrent.rateUpload /1024 > 1024:
-                upload_speed = f"{round(torrent.rateUpload / 1024 / 1024, 2)} MB/s"
-            else:
-                upload_speed = f"{round(torrent.rateUpload / 1024, 2)} KB/s"
-
-            all_torrents += f"`{torrent_name_fixed}`\n*{torrent.progress}% | ↓ {download_speed} | ↑ {upload_speed}*\n\n"
-
-            last_activity = int(datetime.datetime.timestamp(torrent.date_active))
-
-            if time.time() - last_activity < 86400:
-                if torrent.rateDownload > 0:
-                    active_torrents += "⚡️ "
-                elif torrent.progress == 100:
-                    active_torrents += "🎊 "
-                elif torrent.available == 0 and torrent.progress != 100:
-                    active_torrents += "😡 "
-                elif torrent.available > 0 and torrent.progress != 100 and torrent.rateDownload == 0:
-                    active_torrents += "🦦 "
-                torrent_name_fixed = torrent.name.replace(".", " ")
-
-                if torrent.rateDownload /1024 > 1024:
-                    download_speed = f"{round(torrent.rateDownload / 1024 / 1024, 2)} MB/s"
-                else:
-                    download_speed = f"{round(torrent.rateDownload / 1024, 2)} KB/s"
-                if torrent.rateUpload /1024 > 1024:
-                    upload_speed = f"{round(torrent.rateUpload / 1024 / 1024, 2)} MB/s"
-                else:
-                    upload_speed = f"{round(torrent.rateUpload / 1024, 2)} KB/s"
-                
-                active_torrents += f"`{torrent_name_fixed}`\n*{torrent.progress}% | ↓ {download_speed} | ↑ {upload_speed}*\n\n"
-
-        if active_torrents == "Attivi nelle ultime 24 ore:\n\n":
-            active_torrents = "Non ci sono torrent attivi nelle ultime 24 ore."
-                
-        return all_torrents, active_torrents
-
-
-async def view_torrents(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [
-        [
-            InlineKeyboardButton("Refresh", callback_data="refresh"),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    all_torrents, active_torrents = await get_torrent_list()
-
-    if context.args and context.args[0] == "-a":
-        await update.message.reply_text(active_torrents, reply_markup=reply_markup, parse_mode="Markdown")
-    else:
-        await update.message.reply_text(all_torrents, reply_markup=reply_markup, parse_mode="Markdown")
-
-        
-async def add_magnet_download(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    magnet_url = context.args[0]
-    c = Client(host="localhost", port=9091, username="admin", password="Eddy95")
-    torrent = c.add_torrent(magnet_url)
-    await update.message.reply_text(f"ok ho messo a scaricare `{torrent.name}`.", parse_mode="Markdown")
-
-
 async def settings_button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         query = update.callback_query
@@ -565,11 +466,6 @@ async def settings_button(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         print(e)
 
 
-async def restart_bot(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Restarting...")
-    restart()
-
-
 if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler, 0)
@@ -604,10 +500,7 @@ if __name__ == '__main__':
     view_torrents_handler = CommandHandler("torrent", view_torrents)
     application.add_handler(view_torrents_handler, 8)
 
-    add_magnet_download_handler = CommandHandler("add", add_magnet_download)
-    application.add_handler(add_magnet_download_handler, 9)
-
     restart_bot_handler = CommandHandler("restart", restart_bot)
-    application.add_handler(restart_bot_handler, 10)
+    application.add_handler(restart_bot_handler, 9)
 
     application.run_polling(drop_pending_updates=True)
