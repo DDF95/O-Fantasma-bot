7e95a2a84906e3c44789adad9c64fa8147a3ddf2
DDF95 <difebbo.davide@gmail.com>
2023-01-10 23:34:19 +0100

    added transcribe.py

---
+++ transcribe.py
@@ -0,0 +1,43 @@
+from utilities import *
+import speech_recognition
+from pydub import AudioSegment
+
+
+def transcribe_voice(path):
+    new_path = f"{DIRECTORY}/file.wav"
+    AudioSegment.from_file(path).export(new_path, format='wav')
+
+    r = speech_recognition.Recognizer()
+    with speech_recognition.AudioFile(new_path) as source:
+        audio = r.record(source)
+    os.remove(new_path)
+
+    try:
+        return r.recognize_google(audio, language="it-IT")
+    except speech_recognition.UnknownValueError:
+        return '<inaudible>'
+
+
+async def on_voice_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        if "settings" not in context.chat_data or context.chat_data["settings"]["att"] == "✅":
+            if update.message.voice:
+                file = await update.message.voice.get_file()
+                audio = await file.download_to_drive()
+                response = transcribe_voice(audio)
+                if update.message.from_user.language_code == "it":
+                    await context.bot.send_message(update.message.chat.id, f"Trascrizione:\n{response}", reply_to_message_id=update.message.message_id)
+                else:
+                    await context.bot.send_message(update.message.chat.id, f"Transcription:\n{response}", reply_to_message_id=update.message.message_id)
+                os.remove(audio)
+            elif update.message.video_note:
+                file = await update.message.video_note.get_file()
+                audio = await file.download_to_drive()
+                response = transcribe_voice(audio)
+                if update.message.from_user.language_code == "it":
+                    await context.bot.send_message(update.message.chat.id, f"Trascrizione:\n{response}", reply_to_message_id=update.message.message_id)
+                else:
+                    await context.bot.send_message(update.message.chat.id, f"Transcription:\n{response}", reply_to_message_id=update.message.message_id)
+                os.remove(audio)
+    except Exception as e:
+        print(e)
